055f0363303e410ae4ee2b26dba2bd7ce423af21
nima <nimakalhor25@gmail.com>
2023-12-11 09:53:42 +0330

    form submit and validation

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,21 +1,33 @@
-import React from "react";
+import { useRef } from "react";
+import { isCredentialsValid } from "../helper";
 
 function LoginForm() {
+  const usernameInputRef = useRef();
+  const passwordInputRef = useRef();
+
   return (
     <>
       <div className="w-full max-w-md bg-slate-50 border border-slate-200 rounded-lg shadow-md p-6">
         <h2 className="text-2xl font-bold text-gray-900 mb-4">Login</h2>
-        <form className="flex flex-col">
+        <form onSubmit={submitHandler} className="flex flex-col">
           <input
+            ref={usernameInputRef}
             type="username"
             className="bg-gray-100 text-gray-900 border-0 rounded-md p-2 mb-4 focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150"
             placeholder="Username"
           />
           <input
+            ref={passwordInputRef}
             type="password"
             className="bg-gray-100 text-gray-900 border-0 rounded-md p-2 mb-4 focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150"
             placeholder="Password"
           />
+          <div className="bg-red-200 rounded-sm p-2">
+            <p className="text-xs">
+              Invalid username or password. Please check your credentials and
+              try again
+            </p>
+          </div>
           <button
             type="submit"
             className="bg-gradient-to-r from-indigo-500 to-blue-500 text-white font-bold py-2 px-4 rounded-md mt-4 hover:bg-indigo-600 hover:to-blue-600 transition ease-in-out duration-150"
@@ -26,6 +38,14 @@ function LoginForm() {
       </div>
     </>
   );
+
+  function submitHandler() {
+    const username = usernameInputRef.current.value;
+    const password = usernameInputRef.current.value;
+    const isValid = isCredentialsValid(username, password);
+    if (!isValid) return;
+    
+  }
 }
 
 export default LoginForm;
---
+++ src/helper.js
@@ -0,0 +1,6 @@
+import { VALID_USERNAMES } from "./constants";
+
+export function isCredentialsValid(username, password = "") {
+    if(!username || !password) return false
+    return VALID_USERNAMES.includes(username.trim())
+}
